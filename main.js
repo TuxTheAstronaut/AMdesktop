28817ae20d9f373d10c5088a35db36c4b1782032
TuxTheAstronaut <tuxtheastronaut@protonmail.com>
2025-04-18 16:18:26 -0400

    Fixed the Previous button in system tray.

--- main.js
+++ main.js
@@ -145,7 +145,7 @@ function createWindow() {
         click: function () {
           // Ugly code to get the JS path of the Previous button since amp-playback-controls-item-skip.previous does not work
           mainWindow.webContents.executeJavaScript(
-            'document.querySelector("body > div.body-container > div > div.player-bar.player-bar__floating-player.svelte-nrv2cz > div > amp-chrome-player").shadowRoot.querySelector("div > div.chrome-player__playback-controls > apple-music-playback-controls").shadowRoot.querySelector("div > div.music-controls__main > amp-playback-controls-item-skip.previous").click()',
+            'document.querySelector("body > div.body-container > div > div.player-bar.player-bar__floating-player.svelte-1okapwz > div > amp-chrome-player").shadowRoot.querySelector("div > div.chrome-player__playback-controls > apple-music-playback-controls").shadowRoot.querySelector("div > div.music-controls__main > amp-playback-controls-item-skip.previous").click()',
           );
         },
       },
